89a841e80bc8ead3f554e61d162166435d017a26
Dmitry DM <ultrwave@gmail.com>
2021-02-03 11:29:47 +0300

    Header component

---
+++ src/header/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import style from './Header.module.css';
+
+export function Header () {
+    return (
+        <div className={style.Header}>
+
+        </div>
+    );
+}
+
