9a561b1ca4a87e36646a52a40c00a9ea0758f641
Dmitry DM <ultrwave@gmail.com>
2021-02-03 19:34:14 +0300

    nav menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import {Header} from './header/Header';
+import {Nav} from './nav/Nav';
 
 function App() {
     return (
--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import style from './Header.module.css';
+import {Nav} from '../nav/Nav';
 
 export function Header () {
     return (
         <div className={style.Header}>
-
+            <Nav/>
         </div>
     );
 }
